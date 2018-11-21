e2a969b8fb2b5838749f64c2ec38d584536e86cc
Wane Nwani <vickywane@gmail.com>
2018-11-21 02:51:50 -0600

    signed commit

---
+++ greetings.py
@@ -0,0 +1,18 @@
+# simple py file 
+# just a fun stuff
+
+#functions here 
+def intro():
+    print('Welcome to this fun script ' )    
+    intro = input("What is your name  \n "  )
+
+# welcome = input('welcome' ) 
+name = intro 
+
+
+def Greet():
+    print('hello ' + name )   
+        
+# evoking functions 
+intro()
+Greet() 
